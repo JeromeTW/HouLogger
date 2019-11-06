049ed9ad639daf665781e5e71ebcf4d0e9dcc59b
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-11-06 11:25:47 +0800

    實作 Unit Test

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -2,27 +2,86 @@ import XCTest
 import HouLogger
 
 class Tests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  override func setUp() {
+    super.setUp()
+    logTextView.text = ""
+    let fileManager = FileManager.default
+    guard let cachesDirectory = fileManager.cachesDirectory else { return }
+    do {
+      try fileManager.removeItem(at: cachesDirectory)
+    } catch {
+      print(error.localizedDescription)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func test_print_every_level() {
+    logger.configure([.fault, .error, .debug, .info, .normal], shouldShow: true, shouldCache: false)
+    let faultString = "faultString"
+    let errorString = "errorString"
+    let debugString = "debugString"
+    let infoString = "infoString"
+    let normalString = "normalString"
+    logF(faultString)
+    logE(errorString)
+    logD(debugString)
+    logI(infoString)
+    logN(normalString)
+    XCTAssert(logTextView.text.contains(faultString))
+    XCTAssert(logTextView.text.contains(errorString))
+    XCTAssert(logTextView.text.contains(debugString))
+    XCTAssert(logTextView.text.contains(infoString))
+    XCTAssert(logTextView.text.contains(normalString))
+  }
+  
+  func test_print_some_levels() {
+    logger.configure([.fault, .error], shouldShow: true, shouldCache: false)
+    let faultString = "faultString"
+    let errorString = "errorString"
+    let debugString = "debugString"
+    let infoString = "infoString"
+    let normalString = "normalString"
+    logF(faultString)
+    logE(errorString)
+    logD(debugString)
+    logI(infoString)
+    logN(normalString)
+    XCTAssert(logTextView.text.contains(faultString))
+    XCTAssert(logTextView.text.contains(errorString))
+    XCTAssert(logTextView.text.contains(debugString) == false)
+    XCTAssert(logTextView.text.contains(infoString) == false)
+    XCTAssert(logTextView.text.contains(normalString) == false)
+  }
+  
+  func test_log_file_is_existed() {
+    logger.configure([.fault, .error, .debug, .info, .normal], shouldShow: false, shouldCache: true)
+    let faultString = "faultString"
+    let errorString = "errorString"
+    let debugString = "debugString"
+    let infoString = "infoString"
+    let normalString = "normalString"
+    logF(faultString)
+    logE(errorString)
+    logD(debugString)
+    logI(infoString)
+    logN(normalString)
+    let fileManager = FileManager.default
+    guard let cachesDirectory = fileManager.cachesDirectory else { return }
+    let currentDateString = Date().toString(dateFormat: "yyyyMMdd")
+    let filePath = cachesDirectory.appendingPathComponent("\(currentDateString).log")
+
+    XCTAssert(fileManager.fileExists(atPath: filePath.path))
+  }
+  
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure() {
+      // Put the code you want to measure the time of here.
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
 }
