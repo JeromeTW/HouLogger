31869060e801cbe3faef7566de9ebcc3c5ba4dec
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-11-08 07:49:00 +0800

    簡化 LogE 和 LogT 輸入欄位的必要

--- HouLogger/Classes/BaseLogger.swift
+++ HouLogger/Classes/BaseLogger.swift
@@ -98,10 +98,10 @@ public func logC(_ message: Any, file: String = #file, function: String = #funct
   logger.log(message, level: .code, file: file, function: function, line: line)
 }
 
-public func logT(issue: String, message: Any, file: String = #file, function: String = #function, line: Int = #line) {
+public func logT(issue: String = "", message: Any, file: String = #file, function: String = #function, line: Int = #line) {
   logger.log(message, level: .trace(issue: issue), file: file, function: function, line: line)
 }
 
-public func logE(_ message: Any, error: Error, file: String = #file, function: String = #function, line: Int = #line) {
+public func logE(_ message: Any = "", error: Error, file: String = #file, function: String = #function, line: Int = #line) {
   logger.log(message, level: .error(error: error), file: file, function: function, line: line)
 }
